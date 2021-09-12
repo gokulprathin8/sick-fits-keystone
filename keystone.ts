c61973b16a03516730d2e3fbd41493270c679a55
gokulprathin8 <gokulprathin8@gmail.com>
2021-09-12 15:11:49 +0530

    Changes to Keystone TS

--- keystone.ts
+++ keystone.ts
@@ -8,6 +8,7 @@ import { config, createSchema } from '@keystone-next/keystone/schema';
 import { User } from './schemas/User';
 import { Product } from './schemas/Products';
 import { ProductImage } from './schemas/ProductImage';
+import { insertSeedData } from './seed-data';
 
 const databaseURL = process.env.DATABASE_URL;
 
@@ -36,6 +37,12 @@ export default withAuth(
     db: {
       adapter: 'mongoose',
       url: databaseURL,
+      async onConnect(keystone) {
+        console.log('🍓  Connected to Database');
+        if (process.argv.includes('--seed-data')) {
+          await insertSeedData(keystone);
+        }
+      },
     },
     lists: createSchema({
       User,
